8b029e602f3df43d2ad7cc7b1482c9b6328f1ed7
quoctai101 <quoctai101@gmail.com>
2020-01-11 22:46:06 +0700

    - Remove unnecessary files

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    function execHash($providerid, $name, $des, $date)
-    {
-        return hash('sha256', $providerid . $name . $des . $date);
-    }
-
-    echo execHash(1,'asdasd','wfwef',date("Y-m-d H:i:s"));
-?>
