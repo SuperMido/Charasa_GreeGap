452cb645559b5dd9836446dd27c5de4408eacb4d
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-01-11 21:05:23 +0700

    + add approval source

--- views/source/approval.php
+++ views/source/approval.php
@@ -16,12 +16,10 @@
                                             <dd class="col-sm-12"><?=$source["farmer_name"];?></dd>
                                             <dt class="col-sm-12">Farm description:</dt>
                                             <dd class="col-sm-12"><?=$source["farmer_des"];?></dd>
-                                            <dt class="col-sm-12">Farm name:</dt>
-                                            <dd class="col-sm-12"><?=$source["farmer_name"];?></dd>
                                             <dt class="col-sm-12">Item name:</dt>
-                                            <dd class="col-sm-12"><?=$source["sourcename"];?></dd>
+                                            <dd class="col-sm-12"><?=$source["source_name"];?></dd>
                                             <dt class="col-sm-12">Item description:</dt>
-                                            <dd class="col-sm-12"><?=$source["sourcedes"];?></dd>
+                                            <dd class="col-sm-12"><?=$source["source_des"];?></dd>
                                             <dt class="col-sm-12">Item's create date: </dt>
                                             <dd class="col-sm-12"><?=$source["create_at"];?></dd>
                                         </dl>
@@ -29,7 +27,7 @@
                                 </div>
                                 <div class="card-footer">
                                     <!--<td id="genHash" class="text-center align-middle" action="#" method="post" enctype="multipart/form-data">-->
-                                    <a href="./index.php?controller=source&action=approve&id=<?=$source["id"];?>"><button type="button" class="btn btn-primary">
+                                    <a href="./index.php?controller=source&action=approve&id=<?=$source["farm_id"];?>"><button type="button" class="btn btn-primary">
                                             Approve
                                         </button></a>
                                 </div>
